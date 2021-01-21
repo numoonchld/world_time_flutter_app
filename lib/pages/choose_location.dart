9deff7b8d8a3f3388a99d33414d05f30e0484530
numoonchld <raghavendra.saralaya@gmail.com>
2021-01-21 07:58:14 +0530

    removed superflous todo

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -10,7 +10,6 @@ class _ChooseLocationState extends State<ChooseLocation> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     print('Init Sate Ran');
   }
